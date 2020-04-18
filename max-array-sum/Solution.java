8615d7fc5beedb2892357789d3ceddf8bbc700b5
Saurabh Yadav <ysaurabh@gmail.com>
2020-04-18 20:59:19 +0530

    Generated non adjacency data

--- max-array-sum/Solution.java
+++ max-array-sum/Solution.java
@@ -1,3 +1,4 @@
+
 /*
  * Given an array of integers, find the subset of non-adjacent elements with the maximum sum. Calculate the sum of that subset. 
  * https://www.hackerrank.com/challenges/max-array-sum
@@ -5,48 +6,80 @@
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Solution {
 
-    // Complete the maxSubsetSum function below.
-    static int maxSubsetSum(int[] arr) {
-    	int maxSum = 0;
-    	
-    	int[][] nonAdjacentSet = new int[5][5];
-    	
-    	for(int index =0; index < arr.length; index++) {
-    	}
-    	
-    	return maxSum;
+	// Complete the maxSubsetSum function below.
+	static int maxSubsetSum(int[] arr) {
 
-    }
+		List<Integer> arrList = Arrays.stream(arr).boxed().collect(Collectors.toList());
+		System.out.println("Input : " + arrList);	
+		
+		int maxSum = 0;
+		Map<Integer, List<Integer>> adjancency = new HashMap<>();
 
-    private static final Scanner scanner = new Scanner(System.in);
+		for (int index = 0; index < arr.length; index++) {
+			List<Integer> list = new ArrayList<Integer>();
+			if (index != 0) {
+				list.add(arr[index - 1]);
+			}
+			if (index != arr.length - 1) {
+				list.add(arr[index + 1]);
+			}
+			adjancency.put(arr[index], list);
+		}
 
-    public static void main(String[] args) throws IOException {
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+		System.out.println("Adjacency map : " + adjancency);
+		
+		System.out.println("Non Adjacency data is below: ");
 
-        int n = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+		List<List<Integer>> nonAdjaceny = adjancency.entrySet().stream().map(entry -> {
+			List<Integer> cloned = new ArrayList<>(arrList);
+			cloned.remove(entry.getKey());
+			cloned.removeAll(entry.getValue());
+			return cloned;
+		}).collect(Collectors.toList());
+		
+		System.out.println(nonAdjaceny);
+		
+		return maxSum;
+	}
 
-        int[] arr = new int[n];
+	private static final Scanner scanner = new Scanner(System.in);
 
-        String[] arrItems = scanner.nextLine().split(" ");
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+	public static void main(String[] args) throws IOException {
+		BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
-        for (int i = 0; i < n; i++) {
-            int arrItem = Integer.parseInt(arrItems[i]);
-            arr[i] = arrItem;
-        }
+		int n = scanner.nextInt();
+		scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
-        int res = maxSubsetSum(arr);
+		int[] arr = new int[n];
 
-        bufferedWriter.write(String.valueOf(res));
-        bufferedWriter.newLine();
+		String[] arrItems = scanner.nextLine().split(" ");
+		scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
-        bufferedWriter.close();
+		for (int i = 0; i < n; i++) {
+			int arrItem = Integer.parseInt(arrItems[i]);
+			arr[i] = arrItem;
+		}
 
-        scanner.close();
-    }
+		int res = maxSubsetSum(arr);
+
+		bufferedWriter.write(String.valueOf(res));
+		bufferedWriter.newLine();
+
+		bufferedWriter.close();
+
+		scanner.close();
+	}
 }
